036751d2a6f521143d63152a42ade642c6a3577d
Shaonan Zhang <sdotzhang@gmail.com>
2017-08-14 09:22:28 -0700

    Model for list Items and associated migration
    Change-Id: Ie6b243aa5d46cc396add4f8f1c2867a9e77ba405

--- functional_test.py
+++ functional_test.py
@@ -2,7 +2,7 @@
 # @Author: szhang
 # @Date:   2017-08-09 00:31:30
 # @Last Modified by:   Shaonan Zhang
-# @Last Modified time: 2017-08-13 22:43:29
+# @Last Modified time: 2017-08-14 09:22:24
 # tdd w/ python book first file
 
 from selenium import webdriver
@@ -18,6 +18,11 @@ class NewVisitorTestCase(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def _check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows], "[{}] didn't appear in table".format(row_text))
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app, she goes to check out its homepage.
@@ -39,12 +44,15 @@ class NewVisitorTestCase(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.assertTrue(self._check_for_row_in_list_table('1: Buy peacock feathers'))
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.assertTrue(self._check_for_row_in_list_table('1: Buy peacock feathers'))
+        self.assertTrue(self._check_for_row_in_list_table('2: Use peacock feathers to make a fly'))
 
         # The page updates again, and now shows both items on her list
 
