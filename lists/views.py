46a0509e52f79b70b9c00e980999f982ac80e436
Shaonan Zhang <sdotzhang@gmail.com>
2017-08-14 09:32:54 -0700

    use Item model functions
    Change-Id: Ifc2e2bd471c9d92dd2f5446749b25c8fbfaa8cd1

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
+    new_item_text = ''
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+
+    return render(request, 'home.html', {'new_item_text': new_item_text})
