7ec1f3b6f23ace67b9d2ea3e9f0cc173c6b7b247
Shaonan Zhang <sdotzhang@gmail.com>
2017-08-17 09:27:15 -0700

    Redirect after POST, and show all items in template, fix issue with stale elements
    Change-Id: I99c0f358b8f6298073069aefbc7c7f897789497a

--- functional_test.py
+++ functional_test.py
@@ -2,12 +2,15 @@
 # @Author: szhang
 # @Date:   2017-08-09 00:31:30
 # @Last Modified by:   Shaonan Zhang
-# @Last Modified time: 2017-08-14 09:22:24
+# @Last Modified time: 2017-08-17 09:26:41
 # tdd w/ python book first file
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from contextlib import contextmanager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
 
 
 class NewVisitorTestCase(unittest.TestCase):
@@ -19,10 +22,18 @@ class NewVisitorTestCase(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    @contextmanager
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.browser.find_element_by_tag_name("html")
+        yield WebDriverWait(self.browser, timeout).until(
+            staleness_of(old_page)
+        )
+
     def _check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows], "[{}] didn't appear in table".format(row_text))
+        rows_text_list = [row.text for row in rows]
+        self.assertIn(row_text, rows_text_list, "[{}] didn't appear in table".format(row_text))
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app, she goes to check out its homepage.
@@ -44,15 +55,17 @@ class NewVisitorTestCase(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        self.assertTrue(self._check_for_row_in_list_table('1: Buy peacock feathers'))
+        with self.wait_for_page_load(timeout=10):
+            self._check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.assertTrue(self._check_for_row_in_list_table('1: Buy peacock feathers'))
-        self.assertTrue(self._check_for_row_in_list_table('2: Use peacock feathers to make a fly'))
+        with self.wait_for_page_load(timeout=10):
+            self._check_for_row_in_list_table('1: Buy peacock feathers')
+            self._check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # The page updates again, and now shows both items on her list
 
