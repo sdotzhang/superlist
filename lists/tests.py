5b55d2b10cf1e23bf4e5b54183b5db3bf3ec0038
Shaonan Zhang <szhang@brightedge.com>
2017-08-23 08:36:17 -0700

    New model TodoList, different session would have different Items
    Change-Id: I7bacfe3d2838091a456544469903c554755d1c86

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, TodoList
 
 
 class HomePageTest(TestCase):
@@ -19,62 +19,59 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_a_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        new_item_text = 'A new list item'
-        request.POST['item_text'] = new_item_text
-        home_page(request)
 
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, new_item_text)
-
-    def test_home_page_redirects_after_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        response = home_page(request)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/uniq_url/')
-
-    def test_home_page_only_save_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
-
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_item(self):
+        todo_list = TodoList()
+        todo_list.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.todo_list = todo_list
         first_item.save()
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.todo_list = todo_list
         second_item.save()
 
+        saved_first_list = TodoList.objects.first()
+        self.assertEqual(saved_first_list, todo_list)
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.todo_list, todo_list)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.todo_list, todo_list)
 
 
 class ListViewTest(TestCase):
 
     def test_display_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        response = self.client.get('/lists/unique/')
+        todo_list = TodoList.objects.create()
+        Item.objects.create(text='itemey 1', todo_list=todo_list)
+        Item.objects.create(text='itemey 2', todo_list=todo_list)
+        response = self.client.get('/lists/uniq_url/')
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
     def test_uses_list_templates(self):
-        response = self.client.get('/lists/unique/')
+        response = self.client.get('/lists/uniq_url/')
         self.assertTemplateUsed(response, 'list.html')
 
 
+class NewListTest(TestCase):
 
+    def test_saving_a_POST_request(self):
+        new_item_text = 'A new list item'
+        self.client.post('/lists/new', data={'item_text': new_item_text})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, new_item_text)
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/uniq_url/')
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, TodoList
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/uniq_url/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    todo_list = TodoList.objects.create()
+    Item.objects.create(text=request.POST['item_text'], todo_list=todo_list)
+    return redirect('/lists/uniq_url/')
