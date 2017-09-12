ae8b5fb55241fa9a4264adae837bc6ffe11d691e
Shaonan Zhang <sdotzhang@gmail.com>
2017-09-12 09:17:27 -0700

    Split out unit tests into two files
    Change-Id: If8b2df54ee580c28b312e29d649e03e0cc8cf3b4

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -20,34 +20,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_item(self):
-        todo_list = TodoList()
-        todo_list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.todo_list = todo_list
-        first_item.save()
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.todo_list = todo_list
-        second_item.save()
-
-        saved_first_list = TodoList.objects.first()
-        self.assertEqual(saved_first_list, todo_list)
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.todo_list, todo_list)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.todo_list, todo_list)
-
-
 class ListViewTest(TestCase):
 
     def test_displays_only_items_for_that_list(self):
